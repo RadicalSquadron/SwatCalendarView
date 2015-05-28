04c942b321895027a9ea98212513c2e6c2548f2b
IPhone Developer <IphoneDeveloper@IPhones-Mac-mini.local>
2015-05-28 16:15:22 +0530

    Initial Commit

---
+++ SwatCalendarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwatCalendarView
+//
+//  Created by IPhone Developer on 5/28/15.
+//  Copyright (c) 2015 TherasoftOnline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SwatCalendarView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SwatCalendarView
+//
+//  Created by IPhone Developer on 5/28/15.
+//  Copyright (c) 2015 TherasoftOnline. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
